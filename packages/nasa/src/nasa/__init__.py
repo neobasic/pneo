07184821e80c5ac84897740602a89f2b054ae34f
Raul Bras <139993852+teknolista@users.noreply.github.com>
2025-10-14 16:24:45 -0300

    Code formatted and prepared for i18n.

--- packages/nasa/src/nasa/__init__.py
+++ packages/nasa/src/nasa/__init__.py
@@ -1,4 +1,6 @@
-from builtins import _
+from nuke import gettext as _, AppConfig
 
 APP_NAME: str = _("nasa")
-APP_DESC: str = _("Nasa is an implementation of the Language Server Protocol for the NeoBASIC programming language. It provides a set of core features that enhance the developer experience across editors, including code navigation, editing and refactoring tools, diagnostics and linting, and various utilities like semantic tokens for syntax highlighting, inlay hints for inline type information, folding ranges, smart selection ranges, workspace configuration handling, and execution of custom commands such as organizing imports or running tests.")
+APP_DESC: str = _(
+    "Nasa is an implementation of the Language Server Protocol for the NeoBASIC programming language. It provides a set of core features that enhance the developer experience across editors, including code navigation, editing and refactoring tools, diagnostics and linting, and various utilities like semantic tokens for syntax highlighting, inlay hints for inline type information, folding ranges, smart selection ranges, workspace configuration handling, and execution of custom commands such as organizing imports or running tests."
+)
