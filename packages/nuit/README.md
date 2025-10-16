2d1aa6f294c947871c418085a0941fbc97dd3f38
Raul Bras <139993852+teknolista@users.noreply.github.com>
2025-10-16 06:35:51 -0300

    Some minor fixes.

--- packages/nuit/README.md
+++ packages/nuit/README.md
@@ -1,3 +1,3 @@
 # 🌃 NUIT: NeoBASIC Universal Interface for Transpilers
 
-**Nuit** defines a protocol for transpiling NeoBASIC code to other languages.
+**NUIT** defines a protocol for transpiling NeoBASIC code to other languages.
--- packages/nuit/src/nuit/__init__.py
+++ packages/nuit/src/nuit/__init__.py
@@ -2,4 +2,4 @@ from nuke import gettext as _
 
 APP_NAME: str = _("nuit")
 APP_DESC: str = _(
-    "NeoBASIC Universal Interface Transpiler defines a protocol for transpiling NeoBASIC code to other languages.")
+    "NUIT (NeoBASIC Universal Interface for Transpilers) defines a protocol for transpiling NeoBASIC code to other languages.")
