7f5e243298ee13ef31cb67ad018a6579e3ff8979
Raul Bras <139993852+teknolista@users.noreply.github.com>
2025-10-16 06:35:38 -0300

    New package Ntcpp, that implements the Nuit protocol to transpile NeoBASIC into C++.

---
+++ packages/ntcpp/README.md
@@ -0,0 +1,3 @@
+# 🔀 NTCPP: NeoBASIC Transpiler to C++
+
+**NTCPP** implements the NUIT protocol to transpile NeoBASIC code into C++.
---
+++ packages/ntcpp/src/ntcpp/__init__.py
@@ -0,0 +1,5 @@
+from nuke import gettext as _
+
+APP_NAME: str = _("ntcpp")
+APP_DESC: str = _(
+    "NTCPP (NeoBASIC Transpiler to C++) implements the NUIT protocol to transpile NeoBASIC code into C++.")
