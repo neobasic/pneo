6d916637361a084d90b292dc75e4d65c0bd1265e
Raul Bras <139993852+teknolista@users.noreply.github.com>
2025-10-16 06:20:05 -0300

    New package Nuit, that defines a protocol for universal transpiling.

---
+++ packages/nuit/README.md
@@ -0,0 +1,3 @@
+# 🌃 NUIT: NeoBASIC Universal Interface for Transpilers
+
+**Nuit** defines a protocol for transpiling NeoBASIC code to other languages.
---
+++ packages/nuit/src/nuit/__init__.py
@@ -0,0 +1,5 @@
+from nuke import gettext as _
+
+APP_NAME: str = _("nuit")
+APP_DESC: str = _(
+    "NeoBASIC Universal Interface Transpiler defines a protocol for transpiling NeoBASIC code to other languages.")
